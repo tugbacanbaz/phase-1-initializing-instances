3c358e780914a9d1a277581daa31f4c50dd09b37
tugbacanbaz <tugbacanbaz.pi@gmail.com>
2022-12-13 18:47:08 +0300

    Complited Assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your code here
+class Dog {
+    constructor(name, breed) {
+      this.name = name;
+      this.breed = breed;
+    }
+  }
+
+let bigFluffyDog1 = new Dog("Buzz", "greatPyrenees");
+let bigFluffyDog2 = new Dog("Woody", "labrador");
+
+bigFluffyDog1; // => Dog { name: 'Buzz', breed: 'greatPyrenees' }
+bigFluffyDog2; // => Dog { name: 'Woody', breed: 'labrador' }
+
+class Breakfast{
+    constructor(food,drink){
+        this.food=food;
+        this.drink=drink;
+    }
+}
+
+class Lunch{
+    constructor(salad,soup,drink){
+        this.salad=salad;
+        this.soup=soup;
+        this.drink=drink;
+        
+    }
+}
+
+class Dinner{
+    #dessert;
+    constructor(salad,soup,entree,dessert){
+        
+        this.salad=salad;
+        this.soup=soup;
+        this.entree=entree;
+        this.#dessert=dessert;
+    }
+}
